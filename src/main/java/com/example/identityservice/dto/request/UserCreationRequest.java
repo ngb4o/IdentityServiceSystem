969e386bd14a378a0abf6dec43ab4d0a767c009e
haoking1111 <ngbao08052003@gmail.com>
2024-05-24 22:37:21 +0700

    commit

--- src/main/java/com/example/identityservice/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/identityservice/dto/request/UserCreationRequest.java
@@ -1,9 +1,12 @@
 package com.example.identityservice.dto.request;
 
 import jakarta.validation.constraints.Size;
-
+import lombok.Builder;
+import lombok.Data;
 import java.time.LocalDate;
 
+@Data
+@Builder
 public class UserCreationRequest {
 
     @Size(min = 3, message = "USERNAME_INVALID")
@@ -14,44 +17,4 @@ public class UserCreationRequest {
     private String firstName;
     private String lastName;
     private LocalDate dob;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 }

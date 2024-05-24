2b899fdb7cc7b4f4a719409e25e334826905ea8a
ngb4o <ngb08052003@gmail.com>
2024-05-24 22:53:17 +0700

    commit

--- src/main/java/com/example/identityservice/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/identityservice/dto/request/UserCreationRequest.java
@@ -1,20 +1,23 @@
 package com.example.identityservice.dto.request;
 
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
-
     @Size(min = 3, message = "USERNAME_INVALID")
-    private String username;
+    String username;
 
     @Size(min = 8, message = "PASSWORD_INVALID")
-    private String password;
-    private String firstName;
-    private String lastName;
-    private LocalDate dob;
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
 }

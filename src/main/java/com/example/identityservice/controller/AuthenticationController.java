2c6249765549c4a2fece6dce701e7331077a484f
ngb4o <ngb08052003@gmail.com>
2024-05-25 22:56:52 +0700

    final commit

---
+++ src/main/java/com/example/identityservice/controller/AuthenticationController.java
@@ -0,0 +1,16 @@
+package com.example.identityservice.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+
+}
---
+++ src/main/java/com/example/identityservice/service/AuthenticationService.java
@@ -0,0 +1,15 @@
+package com.example.identityservice.service;
+
+import com.example.identityservice.dto.request.AuthenticationRequest;
+import com.example.identityservice.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationService {
+    UserRepository userRepository;
+}
